889cc8ff195a6e81b645d3515291bcc03c02df52
Christiandlarsen <christianelledk@gmail.com>
2016-11-11 14:12:13 +0100

    Testing database.php gitignore

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -4,7 +4,8 @@ class User_model extends CI_Model
 {
     public function get_user()
     {
-
+        $query = $this->db->query('SELECT email AND password FROM users');
+        return $query->result();
     }
 
     public function set_user()
