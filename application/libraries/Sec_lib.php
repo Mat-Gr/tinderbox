54611671d8d38dc3d8593f18ace27c2f997bd353
Mateusz <mateusz.grochala@yahoo.dk>
2016-11-24 14:03:43 +0100

    Added validate function to sec_lib

--- application/libraries/Sec_lib.php
+++ application/libraries/Sec_lib.php
@@ -2,6 +2,29 @@
 
 class Sec_lib
 {
+    public function validate($args, $req) // takes arguments to validate, and requirements for teh validation
+    {
+        foreach($req as $value)
+        {
+            if(!property_exists($args, $value)) // check if the required properties exist
+            {
+                return false;
+            }
+        }
+        foreach($args as $key => $value)
+        {
+            if(!in_array($key, $req)) // check if unwanted properties exist
+            {
+                return false;
+            }
+            elseif(!isset($value) || empty($value) || !is_string($value)) // validate each value
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public function secure($args)
     {
         foreach($args as $key => $value)
