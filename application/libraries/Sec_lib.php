c24ba7cb74759f0ccff05428f6594f7028ad804a
Mateusz <mateusz.grochala@yahoo.dk>
2016-11-24 12:30:26 +0100

    implemented security library

---
+++ application/libraries/Sec_lib.php
@@ -0,0 +1,21 @@
+<?php
+
+class Sec_lib
+{
+    public function secure($args = [])
+    {
+        foreach($args as $key => $value)
+        {
+            if($key == 'password')
+            {
+                $args[$key] = (string)(trim($value));
+            }
+            else
+            {
+                $args[$key] = (string)(trim(strip_tags($value)));
+            }
+        }
+
+        return $args;
+    }
+}
